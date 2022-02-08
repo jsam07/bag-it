c8b23ab403e4fda8454223610a2b9a538b95f75d
Jamie Samuel <jsam07@hotmail.com>
2022-02-07 22:44:31 -0800

    Initial Commit

---
+++ BagIt/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BagIt;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ BagIt/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BagIt;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
