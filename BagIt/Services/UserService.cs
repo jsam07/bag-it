4fd168460dedc491a29de9b87c277f723c5a9c8d
Jamie Samuel <jsam07@hotmail.com>
2022-03-08 02:57:29 -0800

    Remove MongoDB

--- BagIt/Services/UserService.cs
+++ BagIt/Services/UserService.cs
@@ -6,21 +6,5 @@ namespace BagIt.Services;
 
 public class UserService
 {
-    private readonly IMongoCollection<User> _usersCollection;
-
-    public UserService(IOptions<UsersDbSettings> usersDatabaseSettings) {
-        var mongoClient = new MongoClient(
-            usersDatabaseSettings.Value.ConnectionString);
-
-        var mongoDatabase = mongoClient.GetDatabase(
-            usersDatabaseSettings.Value.DatabaseName);
-
-        _usersCollection = mongoDatabase.GetCollection<User>(
-            usersDatabaseSettings.Value.CollectionName);
-    }
-
-    public async Task<List<User>> GetAsync()
-    {
-        return await _usersCollection.Find(_ => true).ToListAsync();
-    }
+    
 }
