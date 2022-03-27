eb5d54bc8fd9779f7081598523d22736be4309cc
iflinda <informlinda@gmail.com>
2022-03-27 16:56:35 -0700

    Added method to add item and delete item

---
+++ BagIt/Models/ShoppingListProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace BagIt.Models;
+
+public class ShoppingListProduct
+{
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    public int ListId { get; set; }
+    public ShoppingList List { get; set; }
+}
--- BagIt/Models/User.cs
+++ BagIt/Models/User.cs
@@ -1,13 +1,35 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace BagIt.Models;
 
-public class User
+
+public class User 
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    [Key]
+    public int UserId { get; set; }
+    
+    [DisplayName("Username")]
+    [MaxLength(70, ErrorMessage="Username must be 70 characters or less")]
+    public string? Username { get; set; }
+    
+    [DisplayName("Email")]
+    [Required(ErrorMessage = "An email address is required.")]
+    public string Email { get; set; }
+    
+    [DisplayName("First Name")]
+    [MaxLength(50, ErrorMessage="First Name must be 50 characters or less")]
     public string? FirstName { get; set; }
+    
+    [DisplayName("Last Name")]
+    [MaxLength(50, ErrorMessage="Last Name must be 50 characters or less")]
     public string? LastName { get; set; }
+    public string? Password { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    
+    public List<UserShoppingList> UserShoppingLists { get; set; }
 }
